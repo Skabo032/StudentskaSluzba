8987d5622fb3d9b7e9dd946a403fab6157be55a6
Ratko Ružičić <rruzicic@gmail.com>
2021-11-13 23:08:54 +0100

    Add getters and setters for Course, Proffesor, Address

--- src/main/Course.java
+++ src/main/Course.java
@@ -17,5 +17,54 @@ public class Course {
 	private List<Student> studentsPassed;
 	private List<Student> studentsFailed;
 	
+	public int getCourseID() {
+		return courseID;
+	}
+	public void setCourseID(int courseID) {
+		this.courseID = courseID;
+	}
+	public int getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(int courseName) {
+		this.courseName = courseName;
+	}
+	public Semester getSemester() {
+		return semester;
+	}
+	public void setSemester(Semester semester) {
+		this.semester = semester;
+	}
+	public int getYearOfStudy() {
+		return yearOfStudy;
+	}
+	public void setYearOfStudy(int yearOfStudy) {
+		this.yearOfStudy = yearOfStudy;
+	}
+	public Professor getCourseProffesor() {
+		return courseProffesor;
+	}
+	public void setCourseProffesor(Professor courseProffesor) {
+		this.courseProffesor = courseProffesor;
+	}
+	public int getEctsPoints() {
+		return ectsPoints;
+	}
+	public void setEctsPoints(int ectsPoints) {
+		this.ectsPoints = ectsPoints;
+	}
+	public List<Student> getStudentsPassed() {
+		return studentsPassed;
+	}
+	public void setStudentsPassed(List<Student> studentsPassed) {
+		this.studentsPassed = studentsPassed;
+	}
+	public List<Student> getStudentsFailed() {
+		return studentsFailed;
+	}
+	public void setStudentsFailed(List<Student> studentsFailed) {
+		this.studentsFailed = studentsFailed;
+	}
+	
 	
 }
