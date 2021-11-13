eeb2244a4b8075c7b6e9c52db01613f3acc34b12
Ratko Ružičić <rruzicic@gmail.com>
2021-11-13 23:17:13 +0100

    Add constructors for Proffesor, Course, Address

--- src/main/Course.java
+++ src/main/Course.java
@@ -17,6 +17,23 @@ public class Course {
 	private List<Student> studentsPassed;
 	private List<Student> studentsFailed;
 	
+	
+	public Course() {
+		super();
+	}
+	public Course(int courseID, int courseName, Semester semester, int yearOfStudy, Professor courseProffesor,
+			int ectsPoints, List<Student> studentsPassed, List<Student> studentsFailed) {
+		super();
+		this.courseID = courseID;
+		this.courseName = courseName;
+		this.semester = semester;
+		this.yearOfStudy = yearOfStudy;
+		this.courseProffesor = courseProffesor;
+		this.ectsPoints = ectsPoints;
+		this.studentsPassed = studentsPassed;
+		this.studentsFailed = studentsFailed;
+	}
+	
 	public int getCourseID() {
 		return courseID;
 	}
