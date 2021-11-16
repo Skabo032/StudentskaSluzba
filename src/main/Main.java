18bf1f4a55fb8c7f3c1f3b997546c5f2ebfc2b8a
Ratko Ružičić <rruzicic@gmail.com>
2021-11-16 20:33:27 +0100

    Add main frame

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,8 +3,9 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		MainFrame mf = new MainFrame();
+		mf.setVisible(true);
 	}
 
 }
---
+++ src/main/MainFrame.java
@@ -0,0 +1,23 @@
+package main;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame{
+	public MainFrame() {
+		super(); // passed no arguments to the parent class
+		Toolkit tKit = Toolkit.getDefaultToolkit();
+		Dimension d = tKit.getScreenSize();
+		
+		int height = d.height;
+		int width = d.width;
+		
+		setSize(width*3/4, height*3/4);
+		setLocation(width*1/8, height*1/8);
+		setTitle("Studentska služba");
+		// must set default close operation because otherwise the window would just APPEAR closed
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
