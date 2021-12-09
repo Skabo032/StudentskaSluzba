622a032c525829d481b089f4742bd141f79c336c
Ratko Ružičić <ruzicic.ra68.2019@uns.ac.rs>
2021-12-09 18:37:50 +0100

    Add dialog boxes and btns to add professor dialog

--- src/view/AddProfessorDialog.java
+++ src/view/AddProfessorDialog.java
@@ -1,9 +1,10 @@
 package view;
 
-import java.awt.FlowLayout;
-import java.awt.Point;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
-import javax.swing.Box;
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -23,22 +24,123 @@ public class AddProfessorDialog extends JDialog{
 		setLocation(mfLocX+ (mfW-sizeX)/2, mfLocY + (mfH - sizeY)/2);
 		
 		
-		JPanel p = new JPanel();
+		GridBagLayout gbl = new GridBagLayout();
+		JPanel p = new JPanel(gbl);
+		GridBagConstraints gb = new GridBagConstraints();
 		
-		JLabel l1 = new JLabel("Ime: ");
+		gb.insets = new Insets(0,0,10,0);	// sets bottom padding to 10px for every component
+		gb.anchor = GridBagConstraints.WEST;
+		
+		// ===== IME =====
+		gb.gridx = 0;
+		gb.gridy = 0;
+		p.add(new JLabel("Ime: "), gb);
+		
 		JTextField firstName = new JTextField(20);
-		l1.setLabelFor(firstName);
-		p.add(l1);
-		p.add(firstName);
+		gb.gridx = 1;
+		gb.gridy = 0;
+		p.add(firstName, gb);
 		
-		p.add(Box.createVerticalGlue());
+		// ===== PREZIME =====
+		gb.gridx = 0;
+		gb.gridy = 1;
+		p.add(new JLabel("Prezime: "), gb);
 		
-		JLabel l2 = new JLabel("Prezime: ");
 		JTextField lastName = new JTextField(20);
-		l2.setLabelFor(lastName);
-		p.add(l2);
-		p.add(lastName);
+		gb.gridx = 1;
+		gb.gridy = 1;
+		p.add(lastName, gb);
 		
+		// ===== DATUM RODJENJA =====
+		gb.gridx = 0;
+		gb.gridy = 2;
+		p.add(new JLabel("Datum rodjenja: "), gb);
+		
+		JTextField dateOfBirth = new JTextField(20);
+		gb.gridx = 1;
+		gb.gridy = 2;
+		p.add(dateOfBirth, gb);
+		
+		// ===== KUCNA ADRESA =====
+		gb.gridx = 0;
+		gb.gridy = 3;
+		p.add(new JLabel("Kucna adresa: "), gb);
+		
+		JTextField homeAddress = new JTextField(20);
+		gb.gridx = 1;
+		gb.gridy = 3;
+		p.add(homeAddress, gb);
+		
+		// ===== BROJ TELEFONA =====
+		gb.gridx = 0;
+		gb.gridy = 4;
+		p.add(new JLabel("Broj telefona: "), gb);
+		
+		JTextField phoneNumber = new JTextField(20);
+		gb.gridx = 1;
+		gb.gridy = 4;
+		p.add(phoneNumber, gb);
+		
+		// ==== EMAIL =====
+		gb.gridx = 0;
+		gb.gridy = 5;
+		p.add(new JLabel("E-mail: "), gb);
+		
+		JTextField email = new JTextField(20);
+		gb.gridx = 1;
+		gb.gridy = 5;
+		p.add(email, gb);
+		
+		// ===== ADRESA KANCELARIJE =====
+		gb.gridx = 0;
+		gb.gridy = 6;
+		p.add(new JLabel("Adresa kancelarije: "), gb);
+		
+		JTextField officeAddress = new JTextField(20);
+		gb.gridx = 1;
+		gb.gridy = 6;
+		p.add(officeAddress, gb);
+		
+		// ===== BROJ LICNE KARTE =====
+		gb.gridx = 0;
+		gb.gridy = 7;
+		p.add(new JLabel("Broj licne karte: "), gb);
+		
+		JTextField idNumber = new JTextField(20);
+		gb.gridx = 1;
+		gb.gridy = 7;
+		p.add(idNumber, gb);
+		
+		// ===== ZVANJE =====
+		gb.gridx = 0;
+		gb.gridy = 8;
+		p.add(new JLabel("Zvanje: "), gb);
+		
+		JTextField title = new JTextField(20);
+		gb.gridx = 1;
+		gb.gridy = 8;
+		p.add(title, gb);
+		
+		// ===== GODINE STAZA =====
+		gb.gridx = 0;
+		gb.gridy = 9;
+		p.add(new JLabel("Godine staza: "), gb);
+		
+		JTextField yearsOfExperience = new JTextField(5);
+		gb.gridx = 1;
+		gb.gridy = 9;
+		p.add(yearsOfExperience, gb);
+		
+		// BUTTONS
+		gb.gridx = 0;
+		gb.gridy = 10;
+		JButton confirm = new JButton("Potvrdi");
+		p.add(confirm, gb);
+		
+		gb.gridx = 1;
+		gb.gridy = 10;
+		JButton cancel = new JButton("Otkazi");
+		p.add(cancel, gb);
 		
 		add(p);
 		setVisible(true);
